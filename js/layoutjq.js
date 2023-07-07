7a9015da27afa59135b53a5379cf23313cd0ca73
yelo-o <flyordig@gmail.com>
2023-07-07 14:02:31 +0900

    레이아웃 부분 수정

--- js/layoutjq.js
+++ js/layoutjq.js
@@ -1,47 +1,51 @@
 const frontURL = 'http://localhost:5500/html'
 const backURL = 'http://localhost:8888/back'
 
-// function showAjax(url, targetObj) {
-//     let xhttp = new XMLHttpRequest();
-//     xhttp.addEventListener('load', (e)=>{
-//             targetObj.innerHTML = e.target.responseText
+//--나의 복잡한 코드--
+// function showAjax(targetUrl, targetObj) {
+//     $.ajax({
+//         url: targetUrl,
+//         method:'get',
+//         success:(responseData)=>{
+//             targetObj.html(responseData)
+//         },
+//         error:(jqXhr, status)=>{
+//             alert(`status:${status}, jqXhr.status:${jqXhr.statusCode}`)
+//         }
 //     })
-//     xhttp.open('get', url)
-//     xhttp.send()
+//     targetObj.load(targetUrl)
 // }
 
+//--강사님의 단순화된 코드--
 function showAjax(targetUrl, targetObj) {
-    $.ajax({
-        url: targetUrl,
-        method:'get',
-        success:(responseData)=>{
-            targetObj.html(responseData)
-        },
-        error:(jqXhr, status)=>{
-            alert(`status:${status}, jqXhr.status:${jqXhr.statusCode}`)
-        }
-    })
+    targetObj.load(targetUrl)
 }
 
 // window.addEventListener('load', () => {
 $(() => {
-    // const menuObjs = document.querySelectorAll('header>nav>ul>li>a')
     const menuObjs = $('header>nav>ul>li>a')
-    // const sectionObj = document.querySelector('section')
     const sectionObj = $('section')
 
+    //--메뉴 클릭될때 할 일 START--
     menuObjs.click((e) => {
-            e.preventDefault()
-            switch (e.target.href) {
-                case `${frontURL}/login.html`:
-                    console.log('로그인 클릭')
-                    showAjax(e.target.href, sectionObj)
-                    break
-                    
-                case `${frontURL}/signup.html`:
-                    showAjax(e.target.href, sectionObj)
-                    console.log('회원가입 클릭')
-                    break
-            }
-        })
+        console.log($(e.target).attr('href'))
+        switch ( $(e.target).attr('href') ) {
+            case `./login.html`://로그인 메뉴
+                console.log('로그인 클릭')
+                showAjax($(e.target).attr('href'), sectionObj)
+                break
+            
+            case `./signup.html`: //가입 메뉴
+                console.log('회원가입 클릭')
+                showAjax($(e.target).attr('href'), sectionObj)
+                break
+
+            case `./productlist.html`: //상품 목록
+                console.log('상품 목록 클릭')
+                location.href = `${frontURL}/productlist.html`
+                break
+        }
+        return false;
     })
+    //--메뉴 클릭될때 할 일 END--
+})
